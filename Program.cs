d52c8e39f8b3977b915608fad74cc455f9bb5e95
winstont14 <116833108+winstont14@users.noreply.github.com>
2023-01-18 20:32:37 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+        {
+            //ถามจำนวนชั้น
+            Console.WriteLine("How many floors?");
+            int floors = int.Parse(Console.ReadLine());
+
+            string print_value = "";
+            //สร้างพีรามิด
+            while(floors >0)
+            {   
+                print_value+= "*" ;
+                Console.WriteLine(print_value);
+                floors--;
+
+
+
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
